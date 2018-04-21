668e50787db77197a8978fff50b6574df66e6630
Shadi Kahwaji <shadik@gmail.comi>
2018-04-21 11:32:53 +0400

    c

--- scrumboard/models.py
+++ scrumboard/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class List(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, related_name="List", on_delete=models.CASCADE, null=True)
-    
+    #
     def __str__(self):
         return "{}".format(self.name)
 
